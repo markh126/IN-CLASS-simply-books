d14ac36cccd2fe6d88e60fc3215f80b85070398a
Mark Hamilton <ham.mark126@gmail.com>
2023-01-18 20:33:40 -0600

    fixed favorite check

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { viewAuthorDetails } from '../../api/mergedData';
+import BookCard from '../../components/BookCard';
 
 export default function ViewAuthors() {
   const [authorDetails, setAuthorDetails] = useState({});
@@ -11,22 +12,33 @@ export default function ViewAuthors() {
   // TODO: grab firebaseKey from url
   const { firebaseKey } = router.query;
 
+  const viewAuthorAndBook = () => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  };
+
   // TODO: make call to API layer to get the data
   useEffect(() => {
-    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+    viewAuthorAndBook();
   }, []);
 
   return (
-    <div className="mt-5 d-flex flex-wrap">
-      <div className="d-flex flex-column">
-        <img src={authorDetails.image} alt={authorDetails.first_name} style={{ width: '300px' }} />
+    <div>
+      <div className="mt-5 d-flex flex-wrap">
+        <div className="d-flex flex-column">
+          <img src={authorDetails.image} alt={authorDetails.first_name} style={{ width: '300px' }} />
+        </div>
+        <div className="text-white ms-5 details">
+          <h2>{authorDetails.first_name} {authorDetails.last_name}
+            {authorDetails.favorite ? ' 🤍' : ''}
+          </h2>
+          Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
+          <hr />
+        </div>
       </div>
-      <div className="text-white ms-5 details">
-        <h2>{authorDetails.first_name} {authorDetails.last_name}
-          {authorDetails.favorite ? ' 🤍' : ''}
-        </h2>
-        Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
-        <hr />
+      <div className="mb-3 d-flex flex-wrap">
+        {authorDetails.books?.map((book) => (
+          <BookCard key={book.firebaseKey} bookObj={book} onUpdate={viewAuthorAndBook} />
+        ))}
       </div>
     </div>
   );
