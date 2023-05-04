97c5455cba674a8ddfff1f4ec0c24fa8161d7b2a
Mark Hamilton <ham.mark126@gmail.com>
2023-05-03 21:10:54 -0500

    python almost full crud

--- pages/book/[firebaseKey].js
+++ pages/book/[firebaseKey].js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { viewBookDetails } from '../../api/mergedData';
 
@@ -16,24 +17,29 @@ export default function ViewBook() {
   }, [firebaseKey]);
 
   return (
-    <div className="mt-5 d-flex flex-wrap">
-      <div className="d-flex flex-column">
-        <img src={bookDetails.image} alt={bookDetails.title} style={{ width: '300px' }} />
+    <>
+      <Head>
+        <title>View {bookDetails.title}</title>
+      </Head>
+      <div className="mt-5 d-flex flex-wrap">
+        <div className="d-flex flex-column">
+          <img src={bookDetails.image} alt={bookDetails.title} style={{ width: '300px' }} />
+        </div>
+        <div className="text-white ms-5 details">
+          <h5>
+            {bookDetails.title} by {bookDetails.authorObject?.first_name} {bookDetails.authorObject?.last_name}
+            {bookDetails.authorObject?.favorite ? ' 🤍' : ''}
+          </h5>
+          Author Email: <a href={`mailto:${bookDetails.authorObject?.email}`}>{bookDetails.authorObject?.email}</a>
+          <p>{bookDetails.description || ''}</p>
+          <hr />
+          <p>
+            {bookDetails.sale
+              ? `🏷️ Sale $${bookDetails.price}`
+              : `$${bookDetails.price}`}
+          </p>
+        </div>
       </div>
-      <div className="text-white ms-5 details">
-        <h5>
-          {bookDetails.title} by {bookDetails.authorObject?.first_name} {bookDetails.authorObject?.last_name}
-          {bookDetails.authorObject?.favorite ? ' 🤍' : ''}
-        </h5>
-        Author Email: <a href={`mailto:${bookDetails.authorObject?.email}`}>{bookDetails.authorObject?.email}</a>
-        <p>{bookDetails.description || ''}</p>
-        <hr />
-        <p>
-          {bookDetails.sale
-            ? `🏷️ Sale $${bookDetails.price}`
-            : `$${bookDetails.price}`}
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
